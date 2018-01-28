7651210b2eb8d5991e61ebd83728453d9c5556e1
Hao Lu <haolu2013@gmail.com>
2018-01-27 22:14:12 -0800

    Added new formatting functions for data

--- app/src/main/java/lu/hao/cryptowatchers/Coin.kt
+++ app/src/main/java/lu/hao/cryptowatchers/Coin.kt
@@ -9,18 +9,32 @@ data class Coin(val id: String,
                 val name: String,
                 val symbol: String,
                 @SerializedName("price_usd") val priceUsd: Double,
+                @SerializedName("24h_volume_usd") val volume: Double,
                 @SerializedName("market_cap_usd") val marketCap: Double,
+                @SerializedName("available_supply") val availableSupply: Double,
+                @SerializedName("max_supply") val maxSupply: Double,
                 @SerializedName("percent_change_1h") val percentChange1h: Double,
                 @SerializedName("percent_change_24h") val percentChange24h: Double,
                 @SerializedName("percent_change_7d") val percentChange7d: Double) : Parcelable {
-    fun formatPrice(price: Double): String = DecimalFormat("$0.00").format(price)
 
-    fun formatMarketCap(marketCap: Double): String = String.format("Market Cap: " + DecimalFormat("$000,000").format(marketCap))
+    fun formatPrice(price: Double): String = DecimalFormat("$###,###.00").format(price)
+
+    fun formatVolume(price: Double): String = DecimalFormat("$###,###").format(price)
+
+    fun formatMarketCap(marketCap: Double): String = String.format("Market Cap: " + DecimalFormat("$###,###").format(marketCap))
+
+    fun formatSupply(supply: Double): String = DecimalFormat("###,###").format(supply)
+
+    // Some coins don't have a finite supply so return the available supply
+    fun formatTotalSupply(supply: Double) = if (supply == 0.0) formatSupply(this.availableSupply) else formatSupply(supply)
 
     constructor(source: Parcel) : this(
             source.readString(),
             source.readString(),
             source.readString(),
+            source.readDouble(),
+            source.readDouble(),
+            source.readDouble(),
             source.readDouble(),
             source.readDouble(),
             source.readDouble(),
@@ -35,7 +49,10 @@ data class Coin(val id: String,
         writeString(name)
         writeString(symbol)
         writeDouble(priceUsd)
+        writeDouble(volume)
         writeDouble(marketCap)
+        writeDouble(availableSupply)
+        writeDouble(maxSupply)
         writeDouble(percentChange1h)
         writeDouble(percentChange24h)
         writeDouble(percentChange7d)
