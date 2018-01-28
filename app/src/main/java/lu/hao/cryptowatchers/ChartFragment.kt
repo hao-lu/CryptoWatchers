e759484b33e9f19944f48c0f16f3185c8a5de784
Hao Lu <haolu2013@gmail.com>
2018-01-27 22:13:41 -0800

    Added chart fragment with tabs for coin details

--- app/src/main/java/lu/hao/cryptowatchers/ChartFragment.kt
+++ app/src/main/java/lu/hao/cryptowatchers/ChartFragment.kt
@@ -8,27 +8,89 @@ import android.view.LayoutInflater
 import android.view.View
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import com.google.gson.GsonBuilder
+import com.robinhood.spark.SparkAdapter
+import com.robinhood.spark.SparkView
+import kotlinx.android.synthetic.main.fragment_chart.*
+import java.text.DecimalFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ChartFragment : Fragment() {
 
     private val TAG = "ChartFragment"
+    private lateinit var mCoinHistory: CoinHistory
 
     override fun onCreateView(inflater: LayoutInflater?,
                               container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater!!.inflate(R.layout.fragment_chart, container, false)
-        val url = arguments.getString("history")
-        Log.d(TAG, url)
+        val period = arguments.getString("period")
+        val coin = arguments.getParcelable<Coin>("coin")
 
-        val observable = CoinCapApi.create().getHistoryObservable("1day", "BTC")
+        val observable = CoinCapApi.create().getHistoryObservable(period, coin.symbol)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
 
         val disposable = observable.subscribe(
-                {history -> Log.d(TAG, "API CALL : " + GsonBuilder().setPrettyPrinting().create().toJson(history)) },
-                {e-> Log.d(TAG, e.message)},
-                {Log.d(TAG, "onComplete")}
+                { history ->
+                    mCoinHistory = history
+                    spark_view.adapter = CoinHistoryAdapter(history)
+                    spark_view.fillType = SparkView.FillType.DOWN
+                    // Initially set the price and percentChange
+                    scrub_y_data.text = coin.formatPrice(coin.priceUsd)
+                    scrub_x_data.text = coin.percentChange1h.toString()
+
+                    spark_view.setScrubListener(
+                            { value ->
+                        if (value == null) {
+                            scrub_y_data.text = coin.formatPrice(coin.priceUsd)
+                            scrub_x_data.text = coin.percentChange1h.toString()
+                        } else {
+                            val pair: Pair<Float, Float> = value as Pair<Float, Float>
+                            val sdf = SimpleDateFormat("MM-dd-yy hh:mm a", Locale.US)
+                            scrub_y_data.text = coin.formatPrice(pair.second.toDouble())
+                            scrub_x_data.text = sdf.format(pair.first)
+                        }
+                    })
+
+                },
+                { e-> Log.d(TAG, "Error: " + e.message) },
+                { Log.d(TAG, "onComplete") }
         )
+
         return rootView
     }
+
+    class CoinHistoryAdapter(history: CoinHistory) : SparkAdapter() {
+        private val xData: FloatArray
+        private val yData: FloatArray
+        private var marketCapHistory: List<List<Float>>
+        private val TAG = "CoinHistoryAdapter"
+
+        init {
+            xData = FloatArray(history.price!!.size)
+            yData = FloatArray(history.price!!.size)
+            marketCapHistory = history.price!!
+            fill()
+        }
+
+        private fun fill() {
+            for (i in 0 until marketCapHistory.size) {
+                xData[i] = marketCapHistory[i][0]
+                yData[i] = marketCapHistory[i][1]
+            }
+        }
+
+        override fun getCount(): Int {
+            return yData.size
+        }
+
+        override fun getItem(index: Int): Any {
+            return Pair(xData[index], yData[index])
+        }
+
+        override fun getY(index: Int): Float {
+            return yData[index]
+        }
+    }
+
 }
